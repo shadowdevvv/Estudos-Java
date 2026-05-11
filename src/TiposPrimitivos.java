da2c5a985b31c5efc6cc6d7bcc42e755ddc4fc65
Bruno <bruno.filadelfo@outlook.com>
2026-05-11 13:38:11 -0300

    Tipos Primitivos

---
+++ src/TiposPrimitivos.java
@@ -0,0 +1,16 @@
+public class TiposPrimitivos {
+  static void main(String[] args) {
+    // Tipos primitivos do java --> int , double , float , char , byte , short , long , boolean//
+
+    String nome = "Bruno";
+    double nota = 8.9;
+    char sala = 'A';
+    int n1 = 15;
+
+    System.out.println("O nome do aluno é: " + nome);
+    System.out.println("A média final da nota é: " + nota);
+    System.out.println("A sala de aula é: Sala " + sala);
+    System.out.println("O número é: " + n1);
+
+  }
+}
