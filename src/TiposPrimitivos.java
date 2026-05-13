ad433f33ae9f2eeeacedc11a9adfa7197ab481fc
Bruno <bruno.filadelfo@outlook.com>
2026-05-13 04:15:14 -0300

    Tipos Primitivos Declaração e tamanho em memória

--- src/TiposPrimitivos.java
+++ src/TiposPrimitivos.java
@@ -4,13 +4,21 @@ public class TiposPrimitivos {
 
     String nome = "Bruno";
     double nota = 8.9;
+    double frequencia = 68.2;
     char sala = 'A';
+    char caractere = 65;
     int n1 = 15;
+    byte quantidadedebyte = 127;
+
 
     System.out.println("O nome do aluno é: " + nome);
     System.out.println("A média final da nota é: " + nota);
+    System.out.println("A média de frequencia do aluno é: " + frequencia);
     System.out.println("A sala de aula é: Sala " + sala);
     System.out.println("O número é: " + n1);
+    System.out.println("Limite de byte: " + quantidadedebyte);
+    System.out.println("char "+ caractere);
+
 
   }
 }
---
+++ src/TiposPrimitivosDeclaracaoeTamanhoemMemoria.java
@@ -0,0 +1,19 @@
+public class TiposPrimitivosDeclaracaoeTamanhoemMemoria {
+
+  public static void main(String[] args) {
+
+    int idade = 29;
+    long numeroGrande = 1000000L;
+    double salarioDouble = 2200;
+    float salarioFloat = 2200f;
+    byte idadeByte = 127;
+    short idadeShort = 29;
+    boolean verdadeiro = true;
+    boolean falso = false;
+    char caracteres = 'B';
+
+    System.out.println("A idade é: " + idadeShort);
+
+  }
+
+}
