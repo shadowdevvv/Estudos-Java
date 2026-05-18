8e25c29bdb4735cbd0c0cdce3e64a97f25c4fd3a
Bruno <bruno.filadelfo@outlook.com>
2026-05-18 13:24:10 -0300

    Operador Relacionais

--- src/OperadoresAritmeticos.java
+++ src/OperadoresAritmeticos.java
@@ -1,8 +1,15 @@
 public class OperadoresAritmeticos {
 
   static void main(String[] args) {
-    int n1 = 15;
+    double n1 = 15;
     int n2 = 30;
+    double total = n1 / n2;
+
+    int b = 15, c = 20;
+    boolean resultado = b > c;
     System.out.println("Valor total " + (n1 + n2));
+    System.out.println("Valor total " + (n2 / n1));
+    System.out.println("resultado: " + resultado);
+    System.out.println(total);
   }
 }
---
+++ src/OperadoresRelacionais.java
@@ -0,0 +1,18 @@
+public class OperadoresRelacionais {
+
+  static void main(String[] args) {
+
+    //%
+
+    int resto = 20 % 7;
+    System.out.println(resto);
+
+    int n1 = 30;
+    int n2 = 40;
+
+    // <> <= >= == !=
+
+    boolean MaiorqueVinte = 30 < 40;
+    System.out.println(MaiorqueVinte);
+  }
+}
